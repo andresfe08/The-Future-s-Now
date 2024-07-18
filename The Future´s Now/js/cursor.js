9b2470513ea1066fb6799e3bee2808bee2f0448a
andresfe08 <96803035+andresfe08@users.noreply.github.com>
2024-07-17 19:47:52 -0500

    Delete The Future´s Now directory
    Hubo una equivocación al subirlo.

--- The Future´s Now/js/cursor.js
+++
@@ -1,8 +0,0 @@
-const cursor = document.querySelector(".cursor");
-
-document.addEventListener("mousemove", (e) => {
-    cursor.style.cssText = `
-    top: ${e.y - 30}px;
-    left: ${e.x - 30}px;
-    `;
-});
